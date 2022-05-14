98f947dd2cc19b8fa73724614f29983797d89d17
Imran Khan <imrankhan.baust.cse16@gmail.com>
2022-05-14 12:36:38 +0600

    test

--- TMSV6/Controllers/UserController.cs
+++ TMSV6/Controllers/UserController.cs
@@ -12,6 +12,7 @@ namespace TMSV6.Controllers
     {
         IUserManager _iuserManager = new UserManager();
         // GET api/<controller>
+        //get All
         [HttpGet]
         public string GetAllEmployee()
         {
